f6ecaaeb1fc844c4289699abc13397f7e5ab4de8
Zaven525 <71457724+Zaven525@users.noreply.github.com>
2025-07-12 22:42:43 +0400

    Add files via upload

--- shared_ptr.cpp
+++ shared_ptr.cpp
@@ -1,97 +1,113 @@
 #include <iostream>
+#include <memory>
 
 template<typename T>
-class SharedPtr{
+class SharedPtr {
 private:
-    T *_data;
-    size_t *_count; //baci datan countnel petqa yndhanur lini shared ptrneri hamar dra hamarel *count
-    void release() {
-        if (_data) {
-            if (--(*_count) == 0) {
-                delete _data;
-                delete _count;
-            }
-        }
-    }
-public:
-    SharedPtr() : _data(nullptr), _count(nullptr) {}
-    explicit SharedPtr(T* data) : _data(data) {_count = new size_t(1); }
-    SharedPtr(const SharedPtr &other) : _data(other._data), _count(other._count) {
-        *_count++;
-    }
-    SharedPtr(SharedPtr &&ohter) noexcept: _data(other._data), _count{other._count; }{
-        other.release();
-    }
-    template<typename U>
-    SharedPtr(std::unique_ptr<U> &&other) {
-        _data = other.release();
-        *_count = 1;
-    }
-    SharedPtr &operator=(const SharedPtr &other) {
-        if (this != &other) {
-            _data = other._data;
-            _count = other._count;
-            ++*_count;
-        }
-        return *this;
-    }
-    SharedPtr &&operator=(SharedPtr &&other) noexcept {
-        if (this != &other) {
-            _data = other._data;
-            _count = other._count;
-            other.release();
-        }
+    T* _data;
+    size_t* _count;
 
+    void release() {
+        if (_count && --(*_count) == 0) {
+            delete _data;
+            delete _count;
+        }
+    }
+
+public:
+    SharedPtr() : _data{nullptr}, _count{nullptr} {}
+
+    explicit SharedPtr(T* data) : _data{data} {
+        _count = new size_t(1);
+    }
+
+    SharedPtr(const SharedPtr& other) : _data{other._data}, _count{other._count} {
+        ++(*_count);
+    }
+
+    SharedPtr(SharedPtr&& other) noexcept : _data{other._data}, _count{other._count} {
+        other._data = nullptr;
+        other._count = nullptr;
+    }
+
+    template<typename U>
+    SharedPtr(std::unique_ptr<U>&& other) {
+        _data = other.release();
+        _count = new size_t(1);
+    }
+
+    SharedPtr& operator=(const SharedPtr& other) {
+        if (this != &other) {
+            release();
+            _data = other._data;
+            _count = other._count;
+            ++(*_count);
+        }
         return *this;
     }
+
+    SharedPtr& operator=(SharedPtr&& other) noexcept {
+        if (this != &other) {
+            release();
+            _data = other._data;
+            _count = other._count;
+            other._data = nullptr;
+            other._count = nullptr;
+        }
+        return *this;
+    }
+
     ~SharedPtr() {
         release();
     }
 
-
-    void reset(T *data) {
+    void reset(T* data) {
         release();
         _data = data;
-        _count = new size_t(new_data ? 1 : 0);
+        _count = new size_t(_data ? 1 : 0);
     }
 
-    void swap(SharedPtr &other) {
+    void swap(SharedPtr& other) {
         std::swap(_data, other._data);
         std::swap(_count, other._count);
     }
 
-    size_t use_count() const{
-        return *(_count);
+    size_t use_count() const {
+        return _count ? *_count : 0;
     }
 
-    T *get() const {
+    T* get() const {
         return _data;
     }
 
-    T &operator*() {
-        return *(_data);
+    T& operator*() {
+        return *_data;
     }
 
-    T *operator->() {
+    T* operator->() {
         return _data;
     }
- 
-    operator bool() const{
-        return data != nullptr;
+
+    operator bool() const {
+        return _data != nullptr;
     }
 
-    bool operator==(const SharedPtr &other) {
+    bool operator==(const SharedPtr& other) const {
         return _data == other._data;
     }
 };
 
-int mian() {
-    SharedPtr <int> ptr1(new int(10));
+int main() {
+    SharedPtr<int> ptr1(new int(10));
     std::cout << *ptr1 << std::endl;
-    std::cout << "count: " << ptr1.use_count();
+    std::cout << "count: " << ptr1.use_count() << std::endl;
 
     auto ptr2 = ptr1;
-    std::cout << "count: " << ptr1.use_count();
+    std::cout << "count after copy: " << ptr1.use_count() << std::endl;
 
-    SharedPtr <int> ptr3(new int(10));
+    SharedPtr<int> ptr3(new int(20));
+    std::cout << *ptr3 << std::endl;
+    std::cout << "count ptr3: " << ptr3.use_count() << std::endl;
+
+    return 0;
 }
